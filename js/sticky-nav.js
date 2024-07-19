729e6d4a7e65d35d37961d12c3d578e8d3422c54
El kajab <kajabikaglody8@gmail.com>
2024-07-20 04:39:27 +0800

    services page

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,12 +1,15 @@
 
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.nav');
+    const hiddenNavBar = document.querySelector('.hiddenNav');
     const scrollPosition = window.scrollY || this.document.documentElement.scrollTop;
     const viewportHeight = window.innerHeight || this.document.documentElement.clientHeight;
 
     if (scrollPosition >= viewportHeight) {
         navbar.classList.add('sticky');
+        hiddenNavBar.classList.remove('hidden');
     } else {
         navbar.classList.remove('sticky');
+        hiddenNavBar.classList.add('hidden');
     }
 });
