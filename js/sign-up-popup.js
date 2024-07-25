eb1923de948261e7b11235ba9c16e26e668c3995
El kajab <kajabikaglody8@gmail.com>
2024-07-26 00:45:05 +0800

    Section Sign up

--- js/sign-up-popup.js
+++ js/sign-up-popup.js
@@ -6,6 +6,8 @@ const btnLogIn = this.document.querySelector('.option--log-in');
 const formBox = this.document.querySelector('.sign-up__box');
 const btnBackSign = document.querySelector('.btn-sign');
 const btnBackLog = document.querySelector('.btn-log');
+const logInSpan = document.querySelector('.log-in__span')
+const signUpSpan = document.querySelector('.sign-up__span')
 
 
 btnSignUp.addEventListener('click', function () {
@@ -25,5 +27,15 @@ btnBackSign.addEventListener('click', function() {
 
 btnBackLog.addEventListener('click', function() {
     formBox.classList.remove('hide');
+    logInPopup.classList.remove('show-popup');
+})
+
+logInSpan.addEventListener('click', function () {
+    logInPopup.classList.add('show-popup');
+    signUpPopup.classList.remove('show-popup');
+})
+
+signUpSpan.addEventListener('click', function () {
+    signUpPopup.classList.add('show-popup');
     logInPopup.classList.remove('show-popup');
 })
